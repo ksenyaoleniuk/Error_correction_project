2a21aaa74a163724d8095ca19d2c54ca0e2fd1b5
ksenyaoleniuk <olenyuk@ucu.edu.ua>
2016-12-05 20:57:46 +0200

    Update test_error_correction.py
    add test

--- test_error_correction.py
+++ test_error_correction.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy
 from Error_correction import ErrorCorrection
 
 
@@ -14,7 +15,28 @@ class TestErrorCorrection(unittest.TestCase):
         result2 = C.str_to_bin(test_str_2)
         self.assertEquals(result, expected)
         self.assertEquals(result2, expected_2)
+        
+    def test_bits_to_str(self):
+        test_str = "01110100011001010111001101110100011010010110111001100111"
+        test_str_2 = "011101000110010101110011011101000110100101101110011001110010000001100011011011110110010001100101"
+        expected = "testing"
+        expected_2 = "testing code"
+        C = ErrorCorrection("data")
+        result = C.bits_to_str(test_str)
+        result2 = C.bits_to_str(test_str_2)
+        self.assertEquals(result, expected)
+        self.assertEquals(result2, expected_2)
 
+    def test_bit_str_to_matrix(self):
+        test_str = "01110100011001010111001101110100011010010110111001100111"
+        test_str_2 = "011101000110010101110011011101000110100101101110011001110010000001100011011011110110010001100101"
+        expected = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0], [1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1], [1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1], [1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1]]
+        expected_2 = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1], [1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0], [1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1]]
+        C = ErrorCorrection("data")
+        result = C.bit_str_to_matrix(test_str)
+        result2 = C.bit_str_to_matrix(test_str_2)
+        self.assertTrue(result.tolist(), expected)
+        self.assertTrue(result2.tolist(), expected_2)
 
 if __name__ == '__main__':
     unittest.main()
